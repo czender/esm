aa890f331ce1f16fa1d1194703cee5118b13dc1a
Charlie Zender <zender@uci.edu>
2025-12-04 12:21:49 -0800

    Define CPLHIST dates before using them

--- run.20251121.v3.LR.1980.I.cld_srt_frn.sh
+++ run.20251121.v3.LR.1980.I.cld_srt_frn.sh
@@ -50,9 +50,12 @@ readonly RTM_INIT_FILE="${RUN_REFDIR}/${RUN_REFCASE}.mosart.r.${RUN_REFDATE}-000
 
 # Settings for I-case forced by ERA5 1980s data
 readonly DATM_MODE="ERA56HR"
-readonly DATM_HIST_YR_ALIGN="${MODEL_START_YEAR}"
-readonly DATM_HIST_YR_START="1980"
-readonly DATM_HIST_YR_END="1989"
+#readonly DATM_HIST_YR_ALIGN="${MODEL_START_YEAR}"
+#readonly DATM_HIST_YR_START="1980"
+#readonly DATM_HIST_YR_END="1989"
+readonly DATM_CPLHIST_YR_ALIGN="${MODEL_START_YEAR}"
+readonly DATM_CPLHIST_YR_START="1980"
+readonly DATM_CPLHIST_YR_END="1989"
 readonly DATM_CO2_TSERIES="20tr"
 readonly DATM_PRESAERO="clim_2000"
 
